c4822b411bd4dbe4d3c82d3516149327cb24c070
Inbatamilan <inbatamilanhk10@gmail.com>
2026-06-19 21:56:34 +0530

    Solved

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const len = string.length;
+  let result = "";
+  for (let i = len - 1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
